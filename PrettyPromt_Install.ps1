a86f26b20dd65266e74f90c4c06df3e8020b4791
W3i537h <maatro_1001@hotmail.com>
2021-09-05 20:52:00 +0200

    Create PrettyPromt_Install.ps1

---
+++ PrettyPromt_Install.ps1
@@ -0,0 +1,3 @@
+Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
+Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
