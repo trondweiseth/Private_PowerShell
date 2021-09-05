7736b6fdb1ce2c095735fd55eced7c18eab91ac5
W3i537h <maatro_1001@hotmail.com>
2021-09-06 01:23:38 +0200

    Update Profile_installer.ps1

--- Profile_installer.ps1
+++ Profile_installer.ps1
@@ -4,7 +4,7 @@ if ($false -eq ([Security.Principal.WindowsIdentity]::GetCurrent().Groups -conta
 #Setting up powershell profile
 Set-ExecutionPolicy -ExecutionPolicy Bypass
 if ($false -eq (Test-Path C:\PSscript\)) {New-Item -Path 'C:\PSscript' -ItemType Directory}
-if ($false -eq (Test-Path C:\PSscript\PsScripts.ps1)) {Move-Item PsScripts.ps1 C:\PSscript\PsScripts.ps1}
+if ($false -eq (Test-Path C:\PSscript\PsScripts.ps1)) {Move-Item PsScripts.ps1 -Destination C:\PSscript\PsScripts.ps1}
 if ($false -eq (Test-Path C:\PSscript\cybercitywp.jpg)) {Move-Item .\cybercitywp.jpg -Destination C:\PSscript\}
 $psprofile = ($PROFILE -split "\\")[-1]
 $profilepath = $PROFILE.Trim($psprofile)
@@ -19,8 +19,7 @@ Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -Skip
 #Downloading and installing Hack font for posh-git and oh-my-posh
 Invoke-WebRequest https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -OutFile .\Hack.zip -UseBasicParsing
 Expand-Archive .\Hack.zip
-cd .\CascadiaCode
-Move-Item .\* C:\Windows\Fonts\
+Get-ChildItem -Path .\Hack -Recurse -File | Move-Item -Destination C:\Windows\Fonts\
 Set-ItemProperty registry::HKEY_CURRENT_USER\Console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe\ -name FaceName -Value "Hack NF"
 Set-ItemProperty registry::HKEY_CURRENT_USER\Console\%SystemRoot%_SysWOW64_WindowsPowerShell_v1.0_powershell.exe\ -Name FaceName -Value "Hack NF"
 
